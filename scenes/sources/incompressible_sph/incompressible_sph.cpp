efca348cc75d429e1cc3d2cb290c5fd3cebf0dce
leo heidelberger <leoheidelberger@leos-MacBook-Pro.local>
2020-03-03 15:45:53 +0100

    small editing

--- scenes/sources/incompressible_sph/incompressible_sph.cpp
+++ scenes/sources/incompressible_sph/incompressible_sph.cpp
@@ -4,111 +4,152 @@
 #include <random>
 
 #ifdef INCOMPRESSIBLE_SPH
-
 using namespace vcl;
 
+// Counter used to save image on hard drive
+int counter_image = 0;
 
-
-
-
-
-
-void scene_model::frame_draw(std::map<std::string,GLuint>& , scene_structure& scene, gui_structure& )
+void scene_model::initialize_sph()
 {
-    float dt = 0.02f;
-    if(! (timer.update()>0) ) dt=0;
+    // Influence distance of a particle (size of the kernel)
+    const float h = 0.1f;
 
+
+    // Rest density (consider 1000 Kg/m^3)
+    const float rho0 = 1000.0f;
+
+    // Stiffness (consider ~2000 - used in tait equation)
+    const float stiffness = 2000.0f;
+
+    // Viscosity parameter
+    const float nu = 2.0f;
+
+    // Total mass of a particle (consider rho0 h^2)
+    const float m = rho0*h*h;
+
+    // Initial particle spacing (relative to h)
+    const float c = 0.95f;
+
+
+    // Fill a square with particles
+    const float epsilon = 1e-3f;
+    for(float x=h; x<1.0f-h; x=x+c*h)
+    {
+        for(float y=-1.0f+h; y<0.0f-h; y=y+c*h)
+        {
+            particle_element particle;
+            particle.p = {x+epsilon*rand_interval(),y,0}; // a zero value in z position will lead to a 2D simulation
+            particles.push_back(particle);
+        }
+    }
+
+    sph_param.rho0 = rho0;
+    sph_param.m    = m;
+}
+
+
+
+void scene_model::frame_draw(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& gui)
+{
+    const float dt = timer.update();
     set_gui();
 
-    create_new_particle();
-    compute_time_step(dt);
+    // Force constant time step
+    float h = dt<=1e-6f? 0.0f : timer.scale*0.0003f;
 
-    display_particles(scene);
-    draw(borders, scene.camera);
-
+    display(shaders, scene, gui);
 }
 
-void scene_model::compute_time_step(float dt)
+void scene_model::setup_data(std::map<std::string,GLuint>& shaders, scene_structure& , gui_structure& gui)
 {
-    // Set forces
-    const size_t N = particles.size();
-    for(size_t k=0; k<N; ++k)
-        particles[k].f = vec3(0,-9.81f,0);
+    gui.show_frame_camera = false;
 
-
-    // Integrate position and speed of particles through time
-    for(size_t k=0; k<N; ++k) {
-        particle_structure& particle = particles[k];
-        vec3& v = particle.v;
-        vec3& p = particle.p;
-        vec3 const& f = particle.f;
-
-        v = (1-0.9f*dt) * v + dt * f; // gravity + friction force
-        p = p + dt * v;
-    }
-
-    // Collisions with cube
-    // ... to do
-
-    // Collisions between spheres
-    // ... to do
-
-}
-
-
-void scene_model::create_new_particle()
-{
-    // Emission of new particle if needed
-    timer.periodic_event_time_step = gui_scene.time_interval_new_sphere;
-    const bool is_new_particle = timer.event;
-    static const std::vector<vec3> color_lut = {{1,0,0},{0,1,0},{0,0,1},{1,1,0},{1,0,1},{0,1,1}};
-
-    if( is_new_particle && gui_scene.add_sphere)
-    {
-        particle_structure new_particle;
-
-        new_particle.r = 0.08f;
-        new_particle.c = color_lut[int(rand_interval()*color_lut.size())];
-
-        // Initial position
-        new_particle.p = vec3(0,0,0);
-
-        // Initial speed
-        const float theta = rand_interval(0, 2*3.14f);
-        new_particle.v = vec3( 2*std::cos(theta), 5.0f, 2*std::sin(theta));
-
-        particles.push_back(new_particle);
-
-    }
-}
-void scene_model::display_particles(scene_structure& scene)
-{
-    const size_t N = particles.size();
-    for(size_t k=0; k<N; ++k)
-    {
-        const particle_structure& part = particles[k];
-
-        sphere.uniform.transform.translation = part.p;
-        sphere.uniform.transform.scaling = part.r;
-        sphere.uniform.color = part.c;
-        draw(sphere, scene.camera);
-    }
-}
-
-
-
-
-void scene_model::setup_data(std::map<std::string,GLuint>& shaders, scene_structure& , gui_structure& )
-{
     sphere = mesh_drawable( mesh_primitive_sphere(1.0f));
     sphere.shader = shaders["mesh"];
+    sphere.uniform.color = {0,0.5,1};
 
-    std::vector<vec3> borders_segments = {{-1,-1,-1},{1,-1,-1}, {1,-1,-1},{1,1,-1}, {1,1,-1},{-1,1,-1}, {-1,1,-1},{-1,-1,-1},
-                                          {-1,-1,1} ,{1,-1,1},  {1,-1,1}, {1,1,1},  {1,1,1}, {-1,1,1},  {-1,1,1}, {-1,-1,1},
-                                          {-1,-1,-1},{-1,-1,1}, {1,-1,-1},{1,-1,1}, {1,1,-1},{1,1,1},   {-1,1,-1},{-1,1,1}};
+    std::vector<vec3> borders_segments = {{-1,-1,-0.1f},{1,-1,-0.1f}, {1,-1,-0.1f},{1,1,-0.1f}, {1,1,-0.1f},{-1,1,-0.1f}, {-1,1,-0.1f},{-1,-1,-0.1f},
+                                          {-1,-1,0.1f} ,{1,-1,0.1f},  {1,-1,0.1f}, {1,1,0.1f},  {1,1,0.1f}, {-1,1,0.1f},  {-1,1,0.1f}, {-1,-1,0.1f},
+                                          {-1,-1,-0.1f},{-1,-1,0.1f}, {1,-1,-0.1f},{1,-1,0.1f}, {1,1,-0.1f},{1,1,0.1f},   {-1,1,-0.1f},{-1,1,0.1f}};
     borders = segments_gpu(borders_segments);
     borders.uniform.color = {0,0,0};
     borders.shader = shaders["curve"];
+
+
+    initialize_sph();
+    initialize_field_image();
+    sphere.uniform.transform.scaling = sph_param.h/5.0f;
+
+    gui_param.display_field = true;
+    gui_param.display_particles = true;
+    gui_param.save_field = false;
+
+}
+
+
+void scene_model::display(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& )
+{
+    draw(borders, scene.camera);
+
+    // Display particles
+    if(gui_param.display_particles)
+    {
+        const size_t N = particles.size();
+        for(size_t k=0; k<N; ++k) {
+            sphere.uniform.transform.translation = particles[k].p;
+            draw(sphere, scene.camera);
+        }
+    }
+
+
+    // Update field image
+    if(gui_param.display_field)
+    {
+        const size_t im_h = field_image.im.height;
+        const size_t im_w = field_image.im.width;
+        std::vector<unsigned char>& im_data = field_image.im.data;
+#pragma omp parallel for
+        for(size_t ky=0; ky<im_h; ++ky)
+        {
+            for(size_t kx=0; kx<im_w; ++kx)
+            {
+                const float x = 2.0f*kx/(im_w-1.0f)-1.0f;
+                const float y = 1.0f-2.0f*ky/(im_h-1.0f);
+
+                const float f = evaluate_display_field({x,y,0.0f});
+                const float value = 0.5f * std::max(f-0.25f,0.0f);
+
+                float r = 1-value;
+                float g = 1-value;
+                float b = 1;
+
+                im_data[4*(kx+im_w*ky)]   = static_cast<unsigned char>(255*std::max(std::min(r,1.0f),0.0f));
+                im_data[4*(kx+im_w*ky)+1] = static_cast<unsigned char>(255*std::max(std::min(g,1.0f),0.0f));
+                im_data[4*(kx+im_w*ky)+2] = static_cast<unsigned char>(255*std::max(std::min(b,1.0f),0.0f));
+                im_data[4*(kx+im_w*ky)+3] = 255;
+            }
+        }
+
+
+
+        // Display texture
+        glBindTexture(GL_TEXTURE_2D, field_image.texture_id);
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0,0, GLsizei(im_w), GLsizei(im_h), GL_RGBA, GL_UNSIGNED_BYTE, &im_data[0]);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        draw(field_image.quad, scene.camera, shaders["mesh"]);
+        glBindTexture(GL_TEXTURE_2D, scene.texture_white);
+
+
+        // Save texture on hard drive
+        if( gui_param.save_field )
+        {
+            const std::string filename = vcl::zero_fill(std::to_string(counter_image),3);
+            image_save_png("output/sph/file_"+filename+".png",field_image.im);
+            ++counter_image;
+        }
+    }
 
 }
 
@@ -117,17 +158,58 @@ void scene_model::setup_data(std::map<std::string,GLuint>& shaders, scene_struct
 void scene_model::set_gui()
 {
     // Can set the speed of the animation
-    ImGui::SliderFloat("Time scale", &timer.scale, 0.05f, 2.0f, "%.2f s");
-    ImGui::SliderFloat("Interval create sphere", &gui_scene.time_interval_new_sphere, 0.05f, 2.0f, "%.2f s");
-    ImGui::Checkbox("Add sphere", &gui_scene.add_sphere);
+    float scale_min = 0.05f;
+    float scale_max = 2.0f;
+    ImGui::SliderScalar("Time scale", ImGuiDataType_Float, &timer.scale, &scale_min, &scale_max, "%.2f s");
 
-    bool stop_anim  = ImGui::Button("Stop"); ImGui::SameLine();
-    bool start_anim = ImGui::Button("Start");
+    ImGui::Checkbox("Display field", &gui_param.display_field);
+    ImGui::Checkbox("Display particles", &gui_param.display_particles);
+    ImGui::Checkbox("Save field on disk", &gui_param.save_field);
 
-    if(stop_anim)  timer.stop();
-    if(start_anim) timer.start();
+    // Start and stop animation
+    if (ImGui::Button("Stop"))
+        timer.stop();
+    if (ImGui::Button("Start"))
+        timer.start();
+
 }
 
+// Fill an image with field computed as a distance function to the particles
+float scene_model::evaluate_display_field(const vcl::vec3& p)
+{
+    float field   = 0.0f;
+    const float d = 0.1f;
+    const size_t N = particles.size();
+    for(size_t i=0; i<N; ++i)
+    {
+        const vec3& pi = particles[i].p;
+        const float r  = norm(p-pi);
+        const float u = r/d;
+        if( u < 4)
+            field += std::exp(-u*u);
+    }
+    return field;
+}
+
+// Initialize an image where local density is displayed
+void scene_model::initialize_field_image()
+{
+    size_t N = 50; // Image dimension
+
+    mesh quad = mesh_primitive_quad({-1,-1,0},{1,-1,0},{1,1,0},{-1,1,0});
+    quad.texture_uv = {{0,1},{1,1},{1,0},{0,0}};
+    field_image.quad = quad;
+
+    field_image.im.width = N;
+    field_image.im.height = N;
+    field_image.im.data.resize(4*field_image.im.width*field_image.im.height);
+    field_image.texture_id = create_texture_gpu(field_image.im);
+    field_image.im.color_type = image_color_type::rgba;
+
+    field_image.quad.uniform.shading.ambiant = 1.0f;
+    field_image.quad.uniform.shading.diffuse = 0.0f;
+    field_image.quad.uniform.shading.specular = 0.0f;
+}
 
 
 
--- scenes/sources/incompressible_sph/incompressible_sph.hpp
+++ scenes/sources/incompressible_sph/incompressible_sph.hpp
@@ -4,43 +4,64 @@
 
 #ifdef INCOMPRESSIBLE_SPH
 
-// Structure of a particle
-struct particle_structure
+// SPH Particle
+struct particle_element
 {
     vcl::vec3 p; // Position
     vcl::vec3 v; // Speed
-    vcl::vec3 f; // Forces
 
-    vcl::vec3 c; // Color
-    float r;     // Radius
+    float rho;
+
+    particle_element() : p{0,0,0},v{0,0,0},a{0,0,0},rho(0),pression(0) {}
 };
 
-struct gui_scene_structure
+// SPH simulation parameters
+struct sph_parameters
 {
-    bool add_sphere = true;
-    float time_interval_new_sphere = 0.5f;
+    float h;     // influence distance of a particle
+    float rho0;  // rest density
+    float m;     // total mass of a particle
 };
+
+// Image used to display the water appearance
+struct field_display
+{
+    vcl::image_raw im;       // Image storage on CPU
+    GLuint texture_id;       // Texture stored on GPU
+    vcl::mesh_drawable quad; // Mesh used to display the texture
+};
+
+// User parameters available in the GUI
+struct gui_parameters
+{
+    bool display_field;
+    bool display_particles;
+    bool save_field;
+};
+
 
 struct scene_model : scene_base
 {
 
+    void initialize_sph();
     void setup_data(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& gui);
     void frame_draw(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& gui);
+    void display(std::map<std::string,GLuint>& shaders, scene_structure& scene, gui_structure& gui);
 
+    std::vector<particle_element> particles;
+    sph_parameters sph_param;
+
+    float evaluate_display_field(const vcl::vec3& p);
+
+    void initialize_field_image();
     void set_gui();
 
-    void compute_time_step(float dt);
-    void create_new_particle();
-    void display_particles(scene_structure& scene);
-
-
-    std::vector<particle_structure> particles;
-
-    vcl::mesh_drawable sphere;      // Visual display of particles
-    vcl::segments_drawable borders; // Visual display of borders
+    gui_parameters gui_param;
+    field_display field_image;
+    vcl::mesh_drawable sphere;
+    vcl::segments_drawable borders;
 
     vcl::timer_event timer;
-    gui_scene_structure gui_scene;
 };
 
 
